5affb9ac4feaf18f2ca232c4cc3c3e9a56ef4342
narakushi <taina.santana1305@gmail.com>
2025-02-11 22:04:18 -0300

    Aula 01 - Interface do aplicativo

---
+++ src/App2.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const App2 = () => {
+  return (
+    <div>App2</div>
+  )
+}
+
+export default App2;
+
+// rafce
+
+// Arrow Function
+// const app2 = () => <h1>Ola, mundo!</h1>;
+// Nomeação de Componente
+// PascalCase
+// Nomeação de Variavel, Função...
+// camelCase 
+
+// Header por exemplo é um componente em PascalCase
+// Export default posso importar com qualquer nome e sem chaves
+//  export sem default só posso importar entre chaves e com o nome exportado
+// Self closing tag : <Header/>
+
+
+// Nomeação de classes em CSS
+// Metodolokgia BEM
+// Blocks
+// Elements
+// Modifiers
+// bloco__elemento--modificador
+//  header
+// header__link
+// header__link--small
+// item-list__header
+
+// Tag vazia em React se chama fragment
