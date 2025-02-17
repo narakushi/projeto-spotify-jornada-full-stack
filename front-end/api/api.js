f49ec5fb8c7e763d15787d952b00ab6a8f081db8
narakushi <taina.santana1305@gmail.com>
2025-02-17 16:26:38 -0300

    Ajustes para o deploy mudando variavel ambiente

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -2,8 +2,8 @@
 import 'dotenv/config'
 import axios from 'axios';
 
-const {NODE_ENV} = process.env;
-const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
+const {VITE_NODE_ENV} = import.meta.env;
+const URL = VITE_NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
